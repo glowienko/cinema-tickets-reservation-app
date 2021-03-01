546df6efe9d3b69f0603b6fddb12d18d3f144407
Patryk Głowienko <pgw@touk.pl>
2021-03-01 20:08:07 +0100

    Base new setup

--- src/main/java/com/tickets/reservationserver/AppRunner.java
+++ src/main/java/com/tickets/reservationserver/AppRunner.java
@@ -1,13 +1,13 @@
-package pl.cinema.server;
+package com.tickets.reservationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerApplication {
+public class AppRunner {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+        SpringApplication.run(AppRunner.class, args);
     }
 
 }
---
+++ src/test/java/com/tickets/reservationserver/AppRunnerTests.java
@@ -0,0 +1,13 @@
+package com.tickets.reservationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRunnerTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
